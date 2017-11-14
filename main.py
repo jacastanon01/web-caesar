40e18ac21be4ce93e5416ee07f64cb8d88e30426
Jacob Castanon <Jacastanon01@gmail.com>
2017-11-13 22:29:54 -0600

    Finished web-caesar

--- main.py
+++ main.py
@@ -10,19 +10,19 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
@@ -31,8 +31,10 @@ form = """
 
                 <input name="rot" type="text" value="0"> 
             </label>
-            <textarea name="text"/> 
+            
+            <textarea name="text"/> {0}
             </textarea>
+            
             <input type="submit" value="Sumbit Query">
         </form>
 
@@ -44,7 +46,7 @@ form = """
 
 @app.route('/')
 def index():
-    return form
+    return form.format('')
 
 @app.route('/', methods=['POST'])
 def encrypt():
@@ -52,7 +54,7 @@ def encrypt():
     rot_value = int(rot_value)
     texts = request.form['text']
     rotated = rotate_string(texts,rot_value)
-    return "<h1>" + rotated + "</h1>"
+    return form.format(rotated)
 
 
 
